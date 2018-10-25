2abeb7174e1e6d84ceb3a6693f2fbbdc88689834
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-25 23:12:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
+  attr_accessor :items, :discount, :total, :last_transaction
+  @@items = []
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+
+  def apply_discount(discount = nil)
+    if discount = true
+      self.total * (discount / 100)
+    end
+  end
+
+  def items(item)
+    @@items <<item
+    @@items
+  end
+
+  def void_last_transaction
+    @@total.pop
+  end
+end
